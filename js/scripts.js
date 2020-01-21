48f74db626d15f758d42b124ab08fdb346a71fc2
Duncan Robbins and Joshua Hellman <joshua@>
2020-01-21 14:21:54 -0800

    closed previous project and created new groceries list using .map and .forEach functions to loop thorough the list and mutate list items

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,16 @@
 $(document).ready(function() {
-  $("#favoritForm").submit(function() {
-    var favorits = [];
-    var leastFav = [];
-    var userInput1 = $("input#item1").val();
-    var userInput2 = $("input#item2").val();
-    var userInput3 = $("input#item3").val();
-    var userInput4 = $("input#item4").val();
-    var userInput5 = $("input#item5").val();
-    var userInput6 = $("input#item6").val();
-    leastFav.push(userInput4, userInput5, userInput6);
-    favorits.push(userInput1, userInput2, userInput3);
-    // leastFav.push(favorits.splice(1, 0, leastFav.toString()).toString())
-    console.log(favorits);
-    console.log(leastFav);
-    $("#resultList").append("<li>" + favorits.splice(0,1)+ "</li>")
-    $("#resultList").append("<li>" + favorits.splice(0,1)+ "</li>")   
-    $("#resultList").append("<li>" + favorits.splice(0,1)+ "</li>")
-    $("#leastResultList").append("<li>" + leastFav.splice(0,1)+ "</li>")
-    $("#leastResultList").append("<li>" + leastFav.splice(0,1)+ "</li>")   
-    $("#leastResultList").append("<li>" + leastFav.splice(0,1)+ "</li>")
-    event.preventDefault();
+  $("#itemForm").submit(function() {
+    $("#formdiv").addClass("none");
+    $("#results").removeClass("none");
+    var items = ["item1","item2","item3","item4","item5","item6"]
+    var newItems = items.map(function(item) {
+      return $("input#" + item).val().toUpperCase()
+    })
+    newItems.sort()
+    newItems.forEach(function(item) {
+      $("#resultList").append("<li>" + item + "</li>")
+    })
+  console.log(newItems)
+  event.preventDefault()
   });
 });
